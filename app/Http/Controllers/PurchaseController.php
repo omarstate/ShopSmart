4bc9e4dae11a784ec3064a8f8e9e21d77f2c735c
omarstate <omarstate@ymail.com>
2025-01-07 00:11:44 +0200

    Updated PurchaseController

---
+++ app/Http/Controllers/PurchaseController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PurchaseController extends Controller
+{
+    public function getPurchases()
+    {
+        $user = Auth::user();
+        $purchases = $user->purchases()->get();
+        return response($purchases);
+    }
+
+
+    public function addPurchase()
+    {
+        $validated = request()->validate([
+            'amount' => ['required', 'numeric', 'min:1'],
+            'purchase_date' => ['required', 'date'],
+        ]);
+
+        $user = Auth::user();
+        $points = ($validated['amount'] / 10);
+        $user->purchases()->create([
+            'user_id' => $user['id'],
+            'amount' => $validated['amount'],
+            'points_earned' => $points,
+            'purchase_date' => $validated['purchase_date']
+        ]);
+
+        $user->update([
+            'points' => $validated['amount']
+        ]);
+
+        return redirect('/profile');
+
+    }
+
+}
